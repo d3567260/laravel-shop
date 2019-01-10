7f9456d0170a9602d5341174ddfb84881099bfb7
chenyucheng <d3567260@gmail.com>
2019-01-10 13:57:17 +0800

    基礎佈局

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      由 <a href="#" target="_blank">YuCheng</a> 設計和編碼 <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+
+    <p class="float-right"><a href="mailto:d3567260@gmail.com">聯絡我們</a></p>
+  </div>
+</footer>
