404ea847426f5862d5fae02f2a4aaa2de18266b6
chenyucheng <d3567260@gmail.com>
2019-02-11 21:50:50 +0800

    創建訂單

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -84,7 +84,7 @@ class Order extends Model
         return $this->hasMany(OrderItem::class);
     }
 
-    protected static function findAvailabelNo()
+    protected static function findAvailableNo()
     {
         $prefix = date('YmdHis');
         for ($i = 0; $i < 10; $i++) {
